adddfa2be5396f585acbe687c29572c3cec8f082
vanita <vanitarautytl@gmail.com>
2022-03-03 18:36:50 +0530

    First Commit

---
+++ src/main/java/mavenproject/Mavenjava/Testselenium.java
@@ -0,0 +1,20 @@
+package mavenproject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Testselenium {
+	
+	@Test
+	public void Test()
+	{
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void Ui()
+	{
+		System.out.println("Helloooo");
+
+	}
+
+}
---
+++ src/test/java/mavenproject/Mavenjava/AppTest.java
@@ -0,0 +1,12 @@
+package mavenproject.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+   @Test
+   public void get()
+   {
+	   System.out.println("Hi");
+   }
+}
