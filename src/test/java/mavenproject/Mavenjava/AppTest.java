ec20f130d3beac90b90580a558b2a876893a7353
vanita <vanitarautytl@gmail.com>
2022-03-04 18:30:38 +0530

    Done all changes again

--- src/test/java/mavenproject/Mavenjava/AppTest.java
+++ src/test/java/mavenproject/Mavenjava/AppTest.java
@@ -11,5 +11,10 @@ public class AppTest
 	   System.out.println("Hi gitchange 2");
 	   System.out.println("Hi gitchange 3");
 	   System.out.println("Hi gitchange 4");
+	   System.out.println("Hi gitchange 5");
+
+	   System.out.println("Hi gitchange 6");
+
    }
+   
 }
